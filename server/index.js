8af66e209d90980a897ae3a8d9f32b5df32f23e2
GonzalezNacho <nacho.ldm63@gmail.com>
2023-10-24 23:05:15 -0300

    funcionando la conexion de socket io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import logger from 'morgan'
+import { Server } from "socket.io";
+import { createServer } from 'node:http'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user has connected!')
+
+    socket.on('disconnect', () => {
+        console.log('an user has disconnected')
+    })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req,res)=> {
@@ -14,6 +27,6 @@ app.get('/style.css', (req,res)=> {
     res.sendFile(process.cwd() + '/client/css/style.css')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
