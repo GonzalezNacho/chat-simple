b95960b3b23724f68c6524a7b2e202a44f71a8f5
GonzalezNacho <nacho.ldm63@gmail.com>
2023-10-26 14:00:12 -0300

    creando base de datos

--- server/index.js
+++ server/index.js
@@ -2,12 +2,27 @@ import express from "express";
 import logger from 'morgan'
 import { Server } from "socket.io";
 import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { createClient } from "@libsql/client";
 
+dotenv.config()
 const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
+
+const db = createClient({
+    url:"libsql://sincere-looker-gonzaleznacho.turso.io",
+    authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+    CREATE TABLE ID NOT EXISTS messages(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        conten TEXT
+    )
+`)
 
 io.on('connection', (socket) => {
     console.log('a user has connected!')
